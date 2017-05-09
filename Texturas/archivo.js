46f244dc2e7a5579889a76f810b0feef55fe8d7f
saavedra017 <pacheko.t.94@hotmail.com>
2017-05-09 13:45:39 -0500

    Update archivo.js

--- Texturas/archivo.js
+++ Texturas/archivo.js
@@ -1,6 +1,6 @@
 function setup(){
 THREE.ImageUtils.crossOrigin='';
-var textura =THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/crate.gif');
+var textura =THREE.ImageUtils.loadTexture('http://saavedra017.github.io/Texturas/crate.gif');
 var material = new THREE.MeshBasicMaterial({map:textura});
 var forma = new THREE.BoxGeometry(1,1,1);
 malla = new THREE.Mesh(forma, material);
