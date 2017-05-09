9be8203583246ed848a31081d22f03d3872274f8
saavedra017 <pacheko.t.94@hotmail.com>
2017-05-09 13:28:10 -0500

    Create archivo.js

---
+++ Texturas/archivo.js
@@ -0,0 +1,30 @@
+function setup(){
+THREE.ImageUtils.crossOrigin='';
+var textura =THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/crate.gif');
+var material = new THREE.MeshBasicMaterial({map:textura});
+var forma = new THREE.BoxGeometry(1,1,1);
+malla = new THREE.Mesh(forma, material);
+
+escena = new THREE.Scene();
+escena.add(malla);
+
+camara = new THREE.Perspective Cámera();
+camara.position.z = 5;
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerHeight*.95, window.innerHight*.95 );
+document.body.appendChild(renderer.domElement);
+}
+
+function loop(){
+requestAnimationFrame(loop);
+
+malla.rotation.x += 0.01;
+malla.rotation.y += 0.01;
+
+renderer.render(escena, camara);
+}
+
+var camara, escena, renderer, malla;
+setup();
+loop();
