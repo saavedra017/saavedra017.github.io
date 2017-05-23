02ddd91ceaabb24870ac40079c7108d634ad577c
saavedra017 <pacheko.t.94@hotmail.com>
2017-05-23 13:51:11 -0500

    Update colision2.js

--- Colisiones/colision2.js
+++ Colisiones/colision2.js
@@ -2,7 +2,7 @@ function setup(){
   cubo1 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
   cubo2 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
   
-  cubo1.potision.x=0.7;
+  cubo1.position.x=0.7;
   cubo2.position.x=-0.7;
   
   camara = new THREE.PerspectiveCamera();
