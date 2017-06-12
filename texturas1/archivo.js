88ac317b5fad532a8949fc0543df8c73cc9a3a80
saavedra017 <pacheko.t.94@hotmail.com>
2017-06-11 23:36:19 -0500

    Update archivo.js

--- texturas1/archivo.js
+++ texturas1/archivo.js
@@ -1,6 +1,6 @@
 function setup(){
 THREE.ImageUtils.crossOrigin='';
-var textura =THREE.ImageUtils.loadTexture('https://saavedra017.github.io/texturas1/MN.jpg');
+var textura =THREE.ImageUtils.loadTexture('https://saavedra017.github.io/texturas1/MPN.jpg');
 var material = new THREE.MeshBasicMaterial({map:textura});
 var forma = new THREE.BoxGeometry(1,1,1);
 malla = new THREE.Mesh(forma, material);
