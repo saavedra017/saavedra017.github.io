e6e740e023f50fe7fbfb456cf959d3fb9c573a08
saavedra017 <pacheko.t.94@hotmail.com>
2017-06-12 00:27:32 -0500

    Create alfil_prueba.js

--- texturas1/alfil_prueba.js
+++ texturas1/alfil_prueba.js
@@ -12,9 +12,19 @@ loader.load(
 	// función cuando la descarga falla
 	function ( xhr ) { var material = new THREE.MeshNormalMaterial(); console.log('fallo la descarga'); }
 	);   */
+
 THREE.ImageUtils.crossOrigin='';
 var textura =THREE.ImageUtils.loadTexture('https://saavedra017.github.io/texturas1/MN.jpg');
 var material = new THREE.MeshBasicMaterial({map:textura});
+
+var iluminacion = new THREE.PointLight(0xFFFFFF);
+iluminacion.position.y = 200;
+
+var base = new THREE.Mesh( new THREE.BoxGeometry(20, 0.1, 20), 
+			   new THREE. MeshLambertMaterial({color: 0xFFFFFF}) );
+base.position.y=0;
+base.rotateX( Math.PI/6 );
+
 
 var puntos = [];
 
@@ -35,21 +45,31 @@ for ( var i = 0; i < 32; i ++ ) {
                      Math.sin(i*0.1)*20, ( i + 70 ) * 2 ) ); 
 }
 
-
 var forma = new THREE.LatheGeometry(puntos);
 
+//var material = new THREE.MeshNormalMaterial();
+
 var malla = new THREE.Mesh( forma, material );
+malla.scale.set(0.1,0.1,0.1);
 malla.rotateX( Math.PI/6 );
 malla.rotateY( Math.PI/6 );
 
 var escena = new THREE.Scene();
 escena.add(malla);
+escena.add(base);
+escena.add(iluminacion);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.y = 50;
-camara.position.z = 500;
+camara.position.y = 10;
+camara.position.z = 50;
 
-var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+var lienzo = document.getElementById("luzPuntual");
+var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+renderizador.setSize(600, 600);
+renderizador.shadowMapEnabled = true;
+malla.castShadow = true;
+base. receiveShadow = true;
+iluminacion.castShadow = true;
+
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
