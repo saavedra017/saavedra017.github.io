9adb5bcb2f76c09d5167617cd87b85cb4306eaa9
saavedra017 <pacheko.t.94@hotmail.com>
2017-06-12 00:34:37 -0500

    Create archivo.js

--- texturas1/archivo.js
+++ texturas1/archivo.js
@@ -1,22 +1,8 @@
 function setup(){
-//THREE.ImageUtils.crossOrigin='';
-//var textura =THREE.ImageUtils.loadTexture('https://saavedra017.github.io/texturas1/MPN.jpg');
-//var material = new THREE.MeshBasicMaterial({map:textura});
-var b_textura = 0;
-var loader = new THREE.TextureLoader();
-do{
-	loader.load(
-	'https://saavedra017.github.io/texturas1/MN.jpg',  
-	// función cuando la textura está cargada
-	function( texture ) { b_textura = 1; var  material = new THREE.MeshBasicMaterial( { map: texture } ); },
-	// función cuando la descarga de a textura está en progreso
-	function ( xhr ){ b_textura = 0; console.log( (xhr.loaded/xhr.total * 100) + '% cargado' ); },
-	// función cuando la descarga falla
-	function ( xhr ) { b_textura = 0; console.log('fallo la descarga'); }
-	); 
-} 
-	while(b_textura === 0 );
-
+THREE.ImageUtils.crossOrigin='';
+var textura =THREE.ImageUtils.loadTexture('https://saavedra017.github.io/texturas1/MPN.jpg');
+var material = new THREE.MeshBasicMaterial({map:textura});
+	
 var forma = new THREE.BoxGeometry(1,1,1);
 malla = new THREE.Mesh(forma, material);
 
